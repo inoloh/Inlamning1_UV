136d6e135f30ea2400fd182af8e2c12c8216aea6
Helena Theise <helena.theise@gmail.com>
2020-10-17 09:44:37 +0200

    changes instance of Guesser to facilitate testing - highest number 10

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,6 +1,8 @@
 public class GuessingGame {
     public static void main(String[] args) {
-        Guesser guess = new Guesser(0,1000);
+
+        //ändrat till högsta siffran 10 istället för 1000 för att enklare kunna testas
+        Guesser guess = new Guesser(0,10);
 
         guess.start();
     }
