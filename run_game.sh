a5b80b0e630373dbfd3d2d589ad18d60bc84b1ee
Helena Theise <helena.theise@gmail.com>
2020-10-17 11:05:54 +0200

    adds a bash script to run the game

---
+++ run_game.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Helena Theise"
+mkdir Helena_Theise_labb
+cp ./*.java  Helena_Theise_labb
+
+cd ~/guessingGame/Helena_Theise_labb
+
+echo "Running game from $(pwd)"
+echo "compiling.."
+javac GuessingGame.java
+echo "running game.."
+java GuessingGame
+echo "Done!"
+echo "Removing class files.."
+rm ./*.class
+ls -1
+
+
